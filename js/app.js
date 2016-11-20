ae61385e201f3cc647f6335e371c8a91d476961d
DimonDevs <dimondevs@gmail.com>
2016-11-20 20:09:14 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+$(function() {
+    $('#search-movie').on('submit', function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var formData = $this.serialize();
+        var formAction = $this.attr('action');
+        var xhr = fetch(formAction + '?' + formData)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            var content = '';
+            $.each(data.results, function(idx, item) {
+                content += '<tr>';
+                content += '<td>' + item.id + '</td>';
+                content += '<td>' + item.original_title + '</td>';
+                content += '<td>' + item.original_language + '</td>';
+                content += '<td>' + idx + '</td>';
+                content += '<td>' + item.vote_average + '</td>';
+                content += '<td>' + item.vote_count + '</td>';
+                content += '<td>' + item.popularity + '</td>';
+                content += '<td>' + item.release_date + '</td>';
+                content += '</tr>';
+            });
+            $('#result tbody').html(content);
+            $('#result').fadeIn();
+        })
+    });
+})
